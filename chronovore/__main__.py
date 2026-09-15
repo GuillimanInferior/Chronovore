9e74880e0d8a022da1a31fbffcd3975e1a6f1988
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2026-09-15 06:51:08 -0400

    Update __main__.py

--- chronovore/__main__.py
+++ chronovore/__main__.py
@@ -50,7 +50,7 @@ app.config["DEBUG_POINTS"]                  =bool(int(environ.get("DEBUG_POINTS"
 #Iterate through all units; this will precompute and save points
 from .helpers.get import get_factions, get_faction
 for faction in get_factions():
-    for unit in get_faction(faction.id).unit_listing:
+    for unit in get_faction(faction['id']).unit_listing:
         pass
 
 @app.before_request
