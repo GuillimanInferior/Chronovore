77d858f81ce6a2688b5105d7398d606ed4a68a77
Roboute Guilliman <92867553+RGuillimanXIII@users.noreply.github.com>
2026-09-16 12:09:55 -0400

    Update data.py

--- chronovore/routes/data.py
+++ chronovore/routes/data.py
@@ -62,7 +62,7 @@ def faction_faction_detachment_detachment_armylist(faction, detachment):
 
     units.sort(key=lambda x:x.name)
 
-    with session['hide_rows']=True:
+    with session['hide_rows'] as True:
         return render_template(
             "army.html",
             f=f,
